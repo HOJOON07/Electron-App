fd30deda7e9893600e6d1794ce98d08feb3c3205
HOJOON07 <ghwns107@naver.com>
2023-09-18 14:09:21 +0900

    electron forge make

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require("electron");
+
+const path = require("node:path");
+
+const createWinddow = () => {
+  const win = new BrowserWindow({
+    width: 1920,
+    height: 1200,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      // dirname 은 실행중인 루트 경로를 가리킨다.
+      //path.join 여러 경로 세그먼트를 함께 결합하여 모든 플랫폼에서 작동하는 경로 문자열을 생성
+    },
+  });
+
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  createWinddow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWinddow();
+  });
+
+  app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") app.quit();
+  });
+});
